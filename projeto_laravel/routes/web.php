23765f779fbd9ed69bb2111dc06b79155bc7c9f8
italorochaj <italorochaj@icloud.com>
2024-09-30 23:16:44 -0300

    Components

--- projeto_laravel/routes/web.php
+++ projeto_laravel/routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 Route::get('/sobre-nos', [SobreNosController::class, 'SobreNos'])->name('site.sobrenos');
 Route::get('/contato', [ContatoController::class, 'Contato'])->name('site.contato');
+Route::post('/contato', [ContatoController::class, 'Contato'])->name('site.contato');
 Route::get('/login', function(){return 'Login';})->name('site.login');
 
 
